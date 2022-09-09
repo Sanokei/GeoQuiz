ab1d4883f06f01b331c50f9881a1fb2050d92d3b
Sano <36809574+Sanokei@users.noreply.github.com>
2022-09-08 22:01:09 -0400

    Homework Zip

--- app/src/main/java/github/sanokei/geoquiz/MainActivity.java
+++ app/src/main/java/github/sanokei/geoquiz/MainActivity.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,34 +11,23 @@ import androidx.coordinatorlayout.widget.CoordinatorLayout;
 
 import com.google.android.material.snackbar.Snackbar;
 
-import github.sanokei.geoquiz.Question;
-
-
 public class MainActivity extends AppCompatActivity {
 
     //data members
     private Button mTrueButton;
     private Button mFalseButton;
     private Boolean mUndo = true;
-    private LinearLayout mLayout;
-    private TextView mQuestionTextView;
-    private Question[] mQuestionBank;
-    private int mCurrentIndex;
-
+    private CoordinatorLayout mLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mQuestionTextView = findViewById(R.id.question_text_view);
-        int question = mQuestionBank[mCurrentIndex].getTextResID();
-        mQuestionTextView.setText(question);
-
         //associate two buttons with the widget on the view of the xml file
         mTrueButton = (Button)findViewById(R.id.true_button);
         mFalseButton = (Button)findViewById(R.id.false_button);
-        mLayout = (LinearLayout)findViewById(R.id.layout);
+        mLayout = (CoordinatorLayout)findViewById(R.id.layout);
 
         Snackbar snackbar = Snackbar.make(mLayout, "You answered the question!",Snackbar.LENGTH_LONG )
                 .setAction("UNDO", new View.OnClickListener() {
@@ -51,17 +39,17 @@ public class MainActivity extends AppCompatActivity {
                 });
 
         mTrueButton.setOnClickListener(v ->
-            {
-//                mTrueButton.setClickable(false);
-                snackbar.show();
-            }
+                {
+                    Snackbar.make(mLayout, R.string.correct_toast, Toast.LENGTH_SHORT).show();
+                    snackbar.show();
+                }
         );
         mFalseButton.setOnClickListener(v ->
-            {
-//                mFalseButton.setClickable(false);
-                snackbar.show();
+                {
+                    Snackbar.make(mLayout, R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
+                    snackbar.show();
 
-            }
+                }
         );
 
         //if the user does not click UNDO, the snackbar dismissess after a couple of second and the answer YES is selected disable the buttons
@@ -81,7 +69,6 @@ public class MainActivity extends AppCompatActivity {
             public void onShown(Snackbar snackbar) {
             }
         });
-
 
     }
 
